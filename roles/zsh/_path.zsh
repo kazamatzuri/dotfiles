759c78b5270dc9290e31047d17bfde52af67803f
Fabian Haupt <fhaupt@wayfair.com>
2022-09-09 11:46:06 -0400

    various updates

--- roles/zsh/_path.zsh
+++ roles/zsh/_path.zsh
@@ -1,3 +1,4 @@
 # Customize to your needs...
 # Make sure to APPEND to the path, so that homebrewed binaries, e.g. /usr/local/bin are found first
 export PATH=${PATH}:$ZSH/bin
+export PATH=${PATH}:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
