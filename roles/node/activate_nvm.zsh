12f31d44fcdd9238e9bd5e1ce248dbe43d3c293c
Fabian Haupt <fhaupt@wayfair.com>
2025-03-25 15:39:18 -0400

    saving things

--- roles/node/activate_nvm.zsh
+++ roles/node/activate_nvm.zsh
@@ -1,4 +1,8 @@
 # Lazily load nvm so that it doesn't slow down shell startup time
 # https://github.com/creationix/nvm/issues/782
 export NVM_DIR="$HOME/.nvm"
-group_lazy_load $HOME/.nvm/nvm.sh nvm node npm npx yarn
+
+# Only set up lazy loading if not already loaded
+if [[ ! -v functions[nvm] ]] && [[ ! -v aliases[nvm] ]]; then
+  group_lazy_load $HOME/.nvm/nvm.sh nvm node npm npx yarn
+fi
